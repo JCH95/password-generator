497a23d53c896848d4aa6eedff60e39f93f43906
JCH95 <jhorger95@gmail.com>
2022-03-03 10:27:23 -0600

    moved vars outside the function

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,8 @@
 // Assignment code here
+var lower = "abcdefghijklmnopqrstuvwxyz";
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var number = "0123456789";
+var symbol = "!@#$%^&*(){}[]<>?,./;:-=_+"
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -8,10 +12,6 @@ function generatePassword() {
   var number = confirm("Confirm if you want your password to contain numbers.");
   var symbol = confirm("Confirm if you want your password to contain special symbols.");
   var length = parseInt(window.prompt("Enter a number between 8-128 for your password length."));
-  var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x" ,"y", "z"];
-  var uppercase = ["A", "B", "C", "D" , "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-  var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "=", "-", "+", "{", "}", "[", "]", "?", "/", ";", ":", ",", "."];
 
   if (length < 8 || length > 128) {
     window.alert("You need to pick a number between 8 and 128!");
